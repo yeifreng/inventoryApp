71e82be9245759d08f75aedab738e78cf22de6ae
Yeifreng <yeifrenjose@gmail.com>
2024-02-09 07:48:04 -0500

    creando componentes de agregar categoria, producto y proveedor

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,21 +3,36 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    HomeComponent
+    HomeComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   exports:[
     SidebarComponent,
-    HomeComponent
+    HomeComponent,
+    SidenavComponent
   ]
 })
 export class SharedModule { }
